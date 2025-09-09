e2a02d0d6efea0f46a5cb9c1e6eff6dbe37fb32b
Sara Baqla <missatrox44@gmail.com>
2025-09-09 13:02:24 -0500

    filter featured projects

--- src/components/sections/FeaturedProjectsSection.tsx
+++ src/components/sections/FeaturedProjectsSection.tsx
@@ -31,7 +31,7 @@ export default function ProjectsSection() {
         </motion.div>
 
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {projects.map((project, index) => (
+          {projects.filter(project => project.featured).map((project, index) => (
             <motion.div
               key={project.id}
               initial={{ opacity: 0, y: 50 }}
